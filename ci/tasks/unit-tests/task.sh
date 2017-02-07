8a2924f7fe651bf9bb009757feaf739b2faaab06
John Calabrese <xchapter7x@gmail.com>
2017-02-07 16:50:15 -0500

    [#138691961] complets first pass of unit test tasks for new tool
    Signed-off-by: alex shumilov <poprygun@hotmail.com>

--- ci/tasks/unit-tests/task.sh
+++ ci/tasks/unit-tests/task.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -ue
-export GOPATH=$PWD/go
+export GOPATH=/go
 export PATH=$GOPATH/bin:$PATH
-cd go/src/github.com/c0-ops/cliaas
 go get github.com/Masterminds/glide
+mkdir -p /go/src/github.com/c0-ops 
+cp -r ./cliaas/ /go/src/github.com/c0-ops/cliaas
+cd /go/src/github.com/c0-ops/cliaas
 glide install
 go test $(glide nv) -v
