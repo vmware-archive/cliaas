500d1784e7d63d391906c177e84c03b1ce5b547d
John Calabrese <jcalabrese@pivotal.io>
2017-02-16 15:22:09 -0500

    [#139414339] add GetVmInfo functionality for google client. add default GCP client connection helper

--- iaas/gcp/gcp_client.go
+++ iaas/gcp/gcp_client.go
@@ -2,10 +2,21 @@ package gcp
 
 import (
 	"fmt"
+	"regexp"
+	"strings"
 
 	errwrap "github.com/pkg/errors"
 	compute "google.golang.org/api/compute/v1"
+
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2/google"
 )
+
+type GoogleComputeClient interface {
+	List(project string, zone string) (*compute.InstanceList, error)
+	Delete(project string, zone string, instance string) (*compute.Operation, error)
+	Insert(project string, zone string, instance *compute.Instance) (*compute.Operation, error)
+}
 
 type ClientAPI interface {
 	CreateVM(instance compute.Instance) error
@@ -15,9 +26,25 @@ type ClientAPI interface {
 }
 
 type GCPClientAPI struct {
-	credPath    string
-	projectName string
-	zoneName    string
+	credPath     string
+	projectName  string
+	zoneName     string
+	googleClient GoogleComputeClient
+}
+
+//NewDefaultGoogleComputeClient -- builds a gcp client which connects to your gcp using `GOOGLE_APPLICATION_CREDENTIALS`
+func NewDefaultGoogleComputeClient(credpath string) (GoogleComputeClient, error) {
+	ctx := context.Background()
+	hc, err := google.DefaultClient(ctx, compute.CloudPlatformScope)
+	if err != nil {
+		return nil, errwrap.Wrap(err, "we have an DefaultClient error")
+	}
+
+	c, err := compute.New(hc)
+	if err != nil {
+		return nil, errwrap.Wrap(err, "we have an compute.New error")
+	}
+	return &googleComputeClientWrapper{instanceService: c.Instances}, nil
 }
 
 func NewGCPClientAPI(configs ...func(*GCPClientAPI) error) (*GCPClientAPI, error) {
@@ -30,16 +57,17 @@ func NewGCPClientAPI(configs ...func(*GCPClientAPI) error) (*GCPClientAPI, error
 		}
 	}
 
-	if gcpClient.credPath == "" {
-		return nil, fmt.Errorf("You have an incomplete GCPClientAPI.credPath")
-	}
-	if gcpClient.projectName == "" {
-		return nil, fmt.Errorf("You have an incomplete GCPClientAPI.projectName")
-	}
-	if gcpClient.zoneName == "" {
-		return nil, fmt.Errorf("You have an incomplete GCPClientAPI.zoneName")
+	if gcpClient.googleClient == nil {
+		return nil, fmt.Errorf("You have an incomplete GCPClientAPI.googleClient")
 	}
 	return gcpClient, nil
+}
+
+func ConfigGoogleClient(value GoogleComputeClient) func(*GCPClientAPI) error {
+	return func(gcpClient *GCPClientAPI) error {
+		gcpClient.googleClient = value
+		return nil
+	}
 }
 
 func ConfigZoneName(value string) func(*GCPClientAPI) error {
@@ -56,13 +84,6 @@ func ConfigProjectName(value string) func(*GCPClientAPI) error {
 	}
 }
 
-func ConfigCredPath(value string) func(*GCPClientAPI) error {
-	return func(gcpClient *GCPClientAPI) error {
-		gcpClient.credPath = value
-		return nil
-	}
-}
-
 func (s *GCPClientAPI) CreateVM(instanceName string, sourceImageTarballUrl string) error {
 	return nil
 }
@@ -71,10 +92,45 @@ func (s *GCPClientAPI) DeleteVM(instanceId uint64) error {
 	return nil
 }
 
+//GetVMInfo - gets the information on the first VM to match the given filter argument
+// currently filter will only do a regex on teh tag||name regex fields against
+// the List's result set
 func (s *GCPClientAPI) GetVMInfo(filter Filter) (*compute.Instance, error) {
-	return nil, nil
+	list, err := s.googleClient.List(s.projectName, s.zoneName)
+	if err != nil {
+		return nil, errwrap.Wrap(err, "call.Do() on google client List() failed")
+	}
+
+	for _, item := range list.Items {
+		var validID = regexp.MustCompile(filter.TagRegexString)
+		var validName = regexp.MustCompile(filter.NameRegexString)
+		taglist := strings.Join(item.Tags.Items, " ")
+		tagMatch := validID.MatchString(taglist)
+		nameMatch := validName.MatchString(item.Name)
+
+		if tagMatch == true && nameMatch == true {
+			return item, nil
+		}
+	}
+	return nil, fmt.Errorf("No instance matches found")
 }
 
 func (s *GCPClientAPI) StopVM(instanceName string) error {
 	return nil
+}
+
+type googleComputeClientWrapper struct {
+	instanceService *compute.InstancesService
+}
+
+func (s *googleComputeClientWrapper) List(project string, zone string) (*compute.InstanceList, error) {
+	return s.instanceService.List(project, zone).Do()
+}
+
+func (s *googleComputeClientWrapper) Delete(project string, zone string, instance string) (*compute.Operation, error) {
+	return s.instanceService.Delete(project, zone, instance).Do()
+}
+
+func (s *googleComputeClientWrapper) Insert(project string, zone string, instance *compute.Instance) (*compute.Operation, error) {
+	return s.instanceService.Insert(project, zone, instance).Do()
 }
--- iaas/gcp/gcp_client_test.go
+++ iaas/gcp/gcp_client_test.go
@@ -2,11 +2,86 @@ package gcp_test
 
 import (
 	. "github.com/c0-ops/cliaas/iaas/gcp"
+	"github.com/c0-ops/cliaas/iaas/gcp/gcpfakes"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	compute "google.golang.org/api/compute/v1"
 )
 
 var _ = Describe("GCPClientAPI", func() {
+
+	Describe("GCPClientAPI", func() {
+		var client *GCPClientAPI
+		var err error
+		var controlZone = "zone"
+		var controlProject = "prj"
+		var controlInstanceName = "blah"
+		var controlInstanceTag = "hello"
+
+		Describe("given a GetVMInfo method and a filter object argument", func() {
+
+			Context("when there is a matching instance", func() {
+				controlInstanceList := createInstanceList(controlInstanceName, controlInstanceTag)
+				BeforeEach(func() {
+					var fakeGoogleClient = new(gcpfakes.FakeGoogleComputeClient)
+					fakeGoogleClient.ListReturns(controlInstanceList, nil)
+
+					client, err = NewGCPClientAPI(
+						ConfigGoogleClient(fakeGoogleClient),
+						ConfigZoneName(controlZone),
+						ConfigProjectName(controlProject),
+					)
+				})
+
+				It("then it should yield the matching gcp instance", func() {
+					inst, err := client.GetVMInfo(Filter{NameRegexString: controlInstanceName, TagRegexString: controlInstanceTag})
+					Expect(inst).ShouldNot(BeNil())
+					Expect(controlInstanceList.Items).To(HaveLen(1))
+					Expect(inst).Should(Equal(controlInstanceList.Items[0]))
+					Expect(err).ShouldNot(HaveOccurred())
+				})
+			})
+
+			Context("when there is no matching instance", func() {
+
+				BeforeEach(func() {
+					var fakeGoogleClient = new(gcpfakes.FakeGoogleComputeClient)
+					fakeGoogleClient.ListReturns(createInstanceList("nothing-to-match", "nothing-to-match"), nil)
+
+					client, err = NewGCPClientAPI(
+						ConfigGoogleClient(fakeGoogleClient),
+						ConfigZoneName(controlZone),
+						ConfigProjectName(controlProject),
+					)
+				})
+
+				It("then it should give an error", func() {
+					inst, err := client.GetVMInfo(Filter{NameRegexString: "bbb", TagRegexString: "ddd"})
+					Expect(inst).Should(BeNil())
+					Expect(err).Should(HaveOccurred())
+				})
+			})
+			Context("when there is empty instance set", func() {
+
+				BeforeEach(func() {
+					var fakeGoogleClient = new(gcpfakes.FakeGoogleComputeClient)
+					fakeGoogleClient.ListReturns(&compute.InstanceList{}, nil)
+
+					client, err = NewGCPClientAPI(
+						ConfigGoogleClient(fakeGoogleClient),
+						ConfigZoneName(controlZone),
+						ConfigProjectName(controlProject),
+					)
+				})
+
+				It("then it should give an error", func() {
+					inst, err := client.GetVMInfo(Filter{})
+					Expect(inst).Should(BeNil())
+					Expect(err).Should(HaveOccurred())
+				})
+			})
+		})
+	})
 
 	Describe("given a NewGCPCLIentAPI()", func() {
 
@@ -16,12 +91,12 @@ var _ = Describe("GCPClientAPI", func() {
 			var err error
 			var controlZone = "zone"
 			var controlProject = "prj"
-			var controlCreds = "/tmp/some.json"
+			var fakeGoogleClient = new(gcpfakes.FakeGoogleComputeClient)
 			BeforeEach(func() {
 				client, err = NewGCPClientAPI(
+					ConfigGoogleClient(fakeGoogleClient),
 					ConfigZoneName(controlZone),
 					ConfigProjectName(controlProject),
-					ConfigCredPath(controlCreds),
 				)
 			})
 			It("then it should provide a properly initialized GCPCLientAPI object", func() {
@@ -31,3 +106,18 @@ var _ = Describe("GCPClientAPI", func() {
 		})
 	})
 })
+
+func createInstanceList(name, tag string) *compute.InstanceList {
+	return &compute.InstanceList{
+		Items: []*compute.Instance{
+			&compute.Instance{
+				Name: name,
+				Tags: &compute.Tags{
+					Items: []string{
+						tag,
+					},
+				},
+			},
+		},
+	}
+}
