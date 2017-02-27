9e191712aaf8da4f47917fb5e17828cec26e7fc4
Kris Hicks <khicks@pivotal.io>
2017-02-27 14:59:29 -0800

    Rejigger pipeline

---
+++ ci/build.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -ex
+
+export GOPATH=$PWD/go
+
+root=$PWD
+output_path=$root/$OUTPUT_PATH
+version=$(cat $root/version/version)
+
+pushd go/src/github.com/c0-ops/cliaas
+  go build \
+    -o $output_path \
+    -ldflags "-s -w -X cliaas.Version=${version}" \
+    github.com/c0-ops/cliaas/cmd/cliaas
+popd
--- ci/tasks/integration_tests/task.sh
+++ ci/tasks/integration_tests/task.sh
@@ -1,9 +1,5 @@
 #!/bin/bash -ue
-export GOPATH=/go
-export PATH=$GOPATH/bin:$PATH
-go get github.com/Masterminds/glide
-mkdir -p /go/src/github.com/c0-ops
-cp -r ./cliaas/ /go/src/github.com/c0-ops/cliaas
-cd /go/src/github.com/c0-ops/cliaas
+
+pushd /go/src/github.com/c0-ops/cliaas
 glide install
 go test ./integration_tests/$IAAS/... -v
