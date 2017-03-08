523596cc7c5e1d7cf846511fbcd813656b233e78
Kris Hicks <khicks@pivotal.io>
2017-03-08 13:58:37 -0800

    More c0-ops -> pivotal-cf

--- ci/tasks/unit_tests/task.sh
+++ ci/tasks/unit_tests/task.sh
@@ -2,7 +2,7 @@
 export GOPATH=/go
 export PATH=$GOPATH/bin:$PATH
 go get github.com/Masterminds/glide
-mkdir -p /go/src/github.com/c0-ops
+mkdir -p /go/src/github.com/pivotal-cf
 cp -r ./cliaas/ /go/src/github.com/pivotal-cf/cliaas
 cd /go/src/github.com/pivotal-cf/cliaas
 glide install
