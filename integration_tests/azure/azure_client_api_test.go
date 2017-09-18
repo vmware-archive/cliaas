81944a8f4c749df3e335832dc7aeab5fe04dbc21
Christian Ang <cang@pivotal.io>
2017-09-18 14:56:13 -0700

    Fix azure integration tests
    [#150067976] pivotal-cf/cliaas #6: AWS replace-vm does not honour IAM Roles

--- integration_tests/azure/azure_client_api_test.go
+++ integration_tests/azure/azure_client_api_test.go
@@ -2,17 +2,13 @@ package azure_test
 
 import (
 	"os"
-	"regexp"
+	"strings"
 
-	"github.com/Azure/azure-sdk-for-go/arm/compute"
-	"github.com/google/uuid"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/pivotal-cf/cliaas/iaas/azure"
 
 	"fmt"
-
-	"github.com/Azure/azure-sdk-for-go/arm/examples/helpers"
 )
 
 const (
@@ -21,198 +17,95 @@ const (
 )
 
 var (
-	controlVMBaseURL     = "https://azureintstore.blob.core.windows.net/opsmanager/"
-	controlVMImageURL    = controlVMBaseURL + "opsmanagerimage193.vhd"
-	subscriptionID       = os.Getenv("AZURE_SUBSCRIPTION_ID")
-	clientID             = os.Getenv("AZURE_CLIENT_ID")
-	clientSecret         = os.Getenv("AZURE_CLIENT_SECRET")
-	tenantID             = os.Getenv("AZURE_TENANT_ID")
-	resourceGroupName    = os.Getenv("AZURE_RESOURCE_GROUP_NAME")
-	nicID                = os.Getenv("AZURE_NIC_ID")
-	storageAccountName   = os.Getenv("AZURE_STORAGE_ACCOUNT_NAME")
-	storageAccountKey    = os.Getenv("AZURE_STORAGE_ACCOUNT_KEY")
-	storageContainerName = os.Getenv("AZURE_STORAGE_CONTAINER_NAME")
-	storageBaseURL       = os.Getenv("AZURE_STORAGE_BASE_URL")
-	adminVMPassword      = os.Getenv("AZURE_VM_ADMIN_PASSWORD")
-	osDiskName           = "linux"
-	vmLocation           = "eastus"
-	vmUser               = "opsmanuser"
-	vmPass               = getGUID()
+	subscriptionID = os.Getenv("AZURE_SUBSCRIPTION_ID")
+	clientID       = os.Getenv("AZURE_CLIENT_ID")
+	clientSecret   = os.Getenv("AZURE_CLIENT_SECRET")
+	tenantID       = os.Getenv("AZURE_TENANT_ID")
+	prefix         = os.Getenv("PREFIX")
+	location       = os.Getenv("LOCATION")
+
+	storageAccountName = strings.Replace(prefix, "-", "", -1)
+	containerName      = "cliaas"
+
+	imageURL = "https://opsmanagerwestus.blob.core.windows.net/images/ops-manager-1.11.11.vhd"
 )
 
 var _ = Describe("Azure API Client", func() {
-	var client compute.VirtualMachinesClient
 	var identifier string
-	var err error
+	var storageAccountKey string
 	var azureClient *azure.Client
+	var testAzureClient azureTestClient
 
 	BeforeEach(func() {
-		client = newClient()
-		identifier = "ops-man-rotation-test-" + getGUID()
-		azureClient, err = azure.NewClient(subscriptionID, clientID, clientSecret, tenantID, resourceGroupName, resourceManagerEndpoint)
-		Expect(err).ShouldNot(HaveOccurred())
-		osDiskImageURL := fmt.Sprintf("%svm-%s.vhd", controlVMBaseURL, getGUID())
-		err = createVM(client, identifier, controlVMImageURL, osDiskImageURL)
+		testAzureClient = azureTestClient{
+			SubscriptionID: subscriptionID,
+			ClientID:       clientID,
+			ClientSecret:   clientSecret,
+			TenantID:       tenantID,
+			Location:       location,
+		}
+
+		resourceGroup := testAzureClient.createResourceGroup(prefix)
+		storageAccountKey = testAzureClient.createStorageAccount(prefix, storageAccountName)
+		createContainer(storageAccountName, storageAccountKey, containerName)
+		newImageURL := copyBlob(storageAccountName, storageAccountKey, containerName, imageURL, "image.vhd")
+
+		subnet := testAzureClient.createVirtualNetwork(prefix, fmt.Sprintf("%s-network", prefix))
+
+		identifier = fmt.Sprintf("%s-vm", prefix)
+		testAzureClient.createVM(prefix, identifier, newImageURL, storageAccountName, containerName, &subnet)
+
+		var err error
+		azureClient, err = azure.NewClient(subscriptionID, clientID, clientSecret, tenantID, *resourceGroup.Name, resourceManagerEndpoint)
 		Expect(err).ShouldNot(HaveOccurred())
 	})
 
 	AfterEach(func() {
 		azureClient.Delete(identifier)
-		Expect(vmExists(client, identifier)).Should(BeFalse(), "this vm should have been removed")
+		Expect(testAzureClient.vmExists(identifier, prefix)).Should(BeFalse())
+		testAzureClient.deleteResourceGroup(prefix)
 	})
 
 	Describe("Delete", func() {
 		JustBeforeEach(func() {
-			Expect(vmExists(client, identifier)).Should(BeTrue(), "does the control test VM exist?")
-			err = azureClient.Delete(identifier)
+			Expect(testAzureClient.vmExists(identifier, prefix)).Should(BeTrue())
+			err := azureClient.Delete(identifier)
 			Expect(err).ShouldNot(HaveOccurred())
 		})
 
 		Context("when called on a vm with a name matching the given regex", func() {
 			It("should delete the matching VM from Azure", func() {
-				Expect(err).ShouldNot(HaveOccurred())
-				Expect(vmExists(client, identifier)).Should(BeFalse(), "was the control test VM removed?")
+				Expect(testAzureClient.vmExists(identifier, prefix)).Should(BeFalse())
 			})
 		})
 	})
 
 	Describe("Replace", func() {
+		var VMID string
 		JustBeforeEach(func() {
-			Expect(vmExists(client, identifier+"_....*")).Should(BeFalse(), "was the control test VM removed?")
-			azureClient.SetStorageContainerName(storageContainerName)
+			Expect(testAzureClient.vmExists(identifier, prefix)).Should(BeTrue())
+
+			vmListResults, err := testAzureClient.getVirtualMachinesClient().List(prefix)
+			VMID = *(*vmListResults.Value)[0].VMID
+
+			azureClient.SetStorageContainerName(containerName)
 			azureClient.SetStorageAccountName(storageAccountName)
-			azureClient.SetStorageBaseURL(storageBaseURL)
-			err = azureClient.SetBlobServiceClient(storageAccountName, storageAccountKey, storageBaseURL)
+			azureClient.SetStorageBaseURL("core.windows.net")
+			err = azureClient.SetBlobServiceClient(storageAccountName, storageAccountKey, "core.windows.net")
 			Expect(err).ShouldNot(HaveOccurred())
+
 			err = azureClient.Replace(identifier, controlOpsManVMDiskURL)
 			Expect(err).ShouldNot(HaveOccurred())
 		})
 
 		Context("when called on a vm with a name matching the given regex", func() {
 			It("should delete the matching VM and spin up a the new VM in its place", func() {
-				Expect(vmExists(client, identifier+"_....*")).Should(BeTrue(), "was the control test VM removed?")
+				vmListResults, err := testAzureClient.getVirtualMachinesClient().List(prefix)
+				Expect(err).ShouldNot(HaveOccurred())
+				newVMID := *(*vmListResults.Value)[0].VMID
+				Expect(testAzureClient.vmExists(identifier, prefix)).Should(BeTrue())
+				Expect(newVMID).NotTo(Equal(VMID))
 			})
 		})
 	})
 })
-
-func createVM(client compute.VirtualMachinesClient, name string, imageURL string, osDiskURL string) error {
-	instance := newVirtualMachine(name, imageURL, osDiskURL)
-	_, err := client.CreateOrUpdate(resourceGroupName, *instance.Name, instance, nil)
-	return err
-}
-
-func newVirtualMachine(name string, vmImageURL string, osDiskURL string) compute.VirtualMachine {
-	tmpName := name
-	tmpImageURL := vmImageURL
-	tmpOsDiskURL := osDiskURL
-	vm := compute.VirtualMachine{
-		Location: &vmLocation,
-		Name:     &tmpName,
-		VirtualMachineProperties: &compute.VirtualMachineProperties{
-			HardwareProfile: &compute.HardwareProfile{
-				VMSize: compute.BasicA0,
-			},
-			OsProfile: &compute.OSProfile{
-				ComputerName:  &vmUser,
-				AdminUsername: &vmUser,
-				AdminPassword: &vmPass,
-			},
-			NetworkProfile: &compute.NetworkProfile{
-				NetworkInterfaces: &[]compute.NetworkInterfaceReference{
-					compute.NetworkInterfaceReference{
-						ID: &nicID,
-					},
-				},
-			},
-			StorageProfile: &compute.StorageProfile{
-				OsDisk: &compute.OSDisk{
-					Name: &osDiskName,
-					Vhd: &compute.VirtualHardDisk{
-						URI: &tmpOsDiskURL,
-					},
-					CreateOption: compute.FromImage,
-					OsType:       compute.Linux,
-					Image: &compute.VirtualHardDisk{
-						URI: &tmpImageURL,
-					},
-				},
-			},
-		},
-	}
-	return vm
-}
-
-func vmExists(client compute.VirtualMachinesClient, identifier string) bool {
-	vmListResults, err := client.List(resourceGroupName)
-	if err != nil {
-		return false
-	}
-
-	var matchingInstances = make([]compute.VirtualMachine, 0)
-	var vmNameFilter = regexp.MustCompile(identifier)
-
-	for vmListResults.Value != nil && len(*vmListResults.Value) > 0 {
-		matchingInstances = getMatchingInstances(*vmListResults.Value, vmNameFilter, matchingInstances)
-		vmListResults, err = client.ListAllNextResults(vmListResults)
-		if err != nil {
-			return false
-		}
-	}
-
-	if len(matchingInstances) == 0 {
-		return false
-	}
-
-	return true
-}
-
-func getMatchingInstances(vmList []compute.VirtualMachine, identifierRegex *regexp.Regexp, matchingInstances []compute.VirtualMachine) []compute.VirtualMachine {
-
-	for _, instance := range vmList {
-		if identifierRegex.MatchString(*instance.Name) {
-			matchingInstances = append(matchingInstances, instance)
-		}
-	}
-	return matchingInstances
-}
-
-func newClient() compute.VirtualMachinesClient {
-	c := map[string]string{
-		"AZURE_CLIENT_ID":                 clientID,
-		"AZURE_CLIENT_SECRET":             clientSecret,
-		"AZURE_SUBSCRIPTION_ID":           subscriptionID,
-		"AZURE_TENANT_ID":                 tenantID,
-		"AZURE_RESOURCE_GROUP_NAME":       resourceGroupName,
-		"AZURE_RESOURCE_MANAGER_ENDPOINT": resourceManagerEndpoint,
-	}
-	if err := checkEnvVar(&c); err != nil {
-		Expect(err).ShouldNot(HaveOccurred())
-	}
-
-	spt, err := helpers.NewServicePrincipalTokenFromCredentials(c, resourceManagerEndpoint)
-	if err != nil {
-		Expect(err).ShouldNot(HaveOccurred())
-	}
-	client := compute.NewVirtualMachinesClient(subscriptionID)
-	client.Authorizer = spt
-	return client
-}
-
-func checkEnvVar(envVars *map[string]string) error {
-	var missingVars []string
-	for varName, value := range *envVars {
-		if value == "" {
-			missingVars = append(missingVars, varName)
-		}
-	}
-	if len(missingVars) > 0 {
-		return fmt.Errorf("Missing environment variables %v", missingVars)
-	}
-	return nil
-}
-
-func getGUID() string {
-	uuid, _ := uuid.NewRandom()
-	return uuid.String()
-}
---
+++ integration_tests/azure/helpers_test.go
@@ -0,0 +1,246 @@
+package azure_test
+
+import (
+	"fmt"
+	"log"
+	"regexp"
+
+	"github.com/Azure/azure-sdk-for-go/arm/compute"
+	"github.com/Azure/azure-sdk-for-go/arm/network"
+	"github.com/Azure/azure-sdk-for-go/arm/resources/resources"
+	armstorage "github.com/Azure/azure-sdk-for-go/arm/storage"
+	"github.com/Azure/azure-sdk-for-go/storage"
+	"github.com/Azure/go-autorest/autorest/azure"
+	"github.com/Azure/go-autorest/autorest/to"
+
+	. "github.com/onsi/gomega"
+)
+
+type azureTestClient struct {
+	TenantID       string
+	ClientID       string
+	ClientSecret   string
+	SubscriptionID string
+	Location       string
+}
+
+func (t azureTestClient) createServicePrincipalToken() *azure.ServicePrincipalToken {
+	oauthConfig, err := azure.PublicCloud.OAuthConfigForTenant(t.TenantID)
+	Expect(err).NotTo(HaveOccurred())
+
+	token, err := azure.NewServicePrincipalToken(*oauthConfig, t.ClientID, t.ClientSecret, azure.PublicCloud.ResourceManagerEndpoint)
+	Expect(err).NotTo(HaveOccurred())
+
+	return token
+}
+
+func (t azureTestClient) getVirtualMachinesClient() compute.VirtualMachinesClient {
+	vmClient := compute.NewVirtualMachinesClient(t.SubscriptionID)
+	vmClient.Authorizer = t.createServicePrincipalToken()
+
+	return vmClient
+}
+
+func (t azureTestClient) createResourceGroup(resourceGroupName string) resources.Group {
+	log.Printf("Creating resource group %s...\n", resourceGroupName)
+	groupClient := resources.NewGroupsClient(t.SubscriptionID)
+	groupClient.Authorizer = t.createServicePrincipalToken()
+
+	group, err := groupClient.CreateOrUpdate(resourceGroupName, resources.Group{
+		Location: &t.Location,
+	})
+	Expect(err).NotTo(HaveOccurred())
+
+	return group
+}
+
+func (t azureTestClient) deleteResourceGroup(resourceGroupName string) {
+	log.Printf("Deleting resource group %s...\n", resourceGroupName)
+	groupClient := resources.NewGroupsClient(t.SubscriptionID)
+	groupClient.Authorizer = t.createServicePrincipalToken()
+
+	_, err := groupClient.Delete(resourceGroupName, nil)
+	Expect(err).NotTo(HaveOccurred())
+}
+
+func (t azureTestClient) createStorageAccount(resourceGroupName, accountName string) string {
+	log.Printf("Creating storage account %s...\n", accountName)
+	accountClient := armstorage.NewAccountsClient(t.SubscriptionID)
+	accountClient.Authorizer = t.createServicePrincipalToken()
+
+	_, err := accountClient.Create(resourceGroupName, accountName, armstorage.AccountCreateParameters{
+		Sku: &armstorage.Sku{
+			Name: armstorage.StandardLRS,
+		},
+		Location: &t.Location,
+		AccountPropertiesCreateParameters: &armstorage.AccountPropertiesCreateParameters{},
+	}, nil)
+	Expect(err).NotTo(HaveOccurred())
+
+	result, err := accountClient.ListKeys(resourceGroupName, accountName)
+	Expect(err).NotTo(HaveOccurred())
+
+	return *(*result.Keys)[0].Value
+}
+
+func (t azureTestClient) createVirtualNetwork(resourceGroupName, networkName string) network.Subnet {
+	log.Printf("Creating virtual network %s...\n", networkName)
+	vNetClient := network.NewVirtualNetworksClient(t.SubscriptionID)
+	subnetClient := network.NewSubnetsClient(t.SubscriptionID)
+
+	token := t.createServicePrincipalToken()
+	vNetClient.Authorizer = token
+	subnetClient.Authorizer = token
+
+	_, err := vNetClient.CreateOrUpdate(resourceGroupName, networkName, network.VirtualNetwork{
+		Location: &t.Location,
+		VirtualNetworkPropertiesFormat: &network.VirtualNetworkPropertiesFormat{
+			AddressSpace: &network.AddressSpace{
+				AddressPrefixes: &[]string{"10.0.0.0/16"},
+			},
+		},
+	}, nil)
+	Expect(err).NotTo(HaveOccurred())
+
+	log.Printf("Creating default subnet...\n")
+	_, err = subnetClient.CreateOrUpdate(resourceGroupName, networkName, "default", network.Subnet{
+		SubnetPropertiesFormat: &network.SubnetPropertiesFormat{
+			AddressPrefix: to.StringPtr("10.0.0.0/24"),
+		},
+	}, nil)
+	Expect(err).NotTo(HaveOccurred())
+
+	defaultSubnet, err := subnetClient.Get(resourceGroupName, networkName, "default", "")
+	Expect(err).NotTo(HaveOccurred())
+
+	return defaultSubnet
+}
+
+func (t azureTestClient) createVM(resourceGroupName, name, imageURL, accountName, containerName string, subnet *network.Subnet) {
+	log.Printf("Creating vm %s...\n", name)
+	vmClient := compute.NewVirtualMachinesClient(t.SubscriptionID)
+	vmClient.Authorizer = t.createServicePrincipalToken()
+
+	nicParameters := t.createNIC(resourceGroupName, name, subnet)
+	osDiskImageURL := fmt.Sprintf("https://%s.blob.core.windows.net/%s/%s.vhd", accountName, containerName, name)
+
+	_, err := vmClient.CreateOrUpdate(resourceGroupName, name, compute.VirtualMachine{
+		Location: &t.Location,
+		VirtualMachineProperties: &compute.VirtualMachineProperties{
+			HardwareProfile: &compute.HardwareProfile{
+				VMSize: compute.StandardDS1,
+			},
+			StorageProfile: &compute.StorageProfile{
+				OsDisk: &compute.OSDisk{
+					Name: to.StringPtr("osDisk"),
+					Vhd: &compute.VirtualHardDisk{
+						URI: &osDiskImageURL,
+					},
+					CreateOption: compute.FromImage,
+					OsType:       compute.Linux,
+					Image: &compute.VirtualHardDisk{
+						URI: &imageURL,
+					},
+				},
+			},
+			OsProfile: &compute.OSProfile{
+				ComputerName:  &name,
+				AdminUsername: to.StringPtr("notadmin"),
+				AdminPassword: to.StringPtr("Pa$$w0rd1975"),
+			},
+			NetworkProfile: &compute.NetworkProfile{
+				NetworkInterfaces: &[]compute.NetworkInterfaceReference{
+					{
+						ID: nicParameters.ID,
+						NetworkInterfaceReferenceProperties: &compute.NetworkInterfaceReferenceProperties{
+							Primary: to.BoolPtr(true),
+						},
+					},
+				},
+			},
+		},
+	}, nil)
+	Expect(err).NotTo(HaveOccurred())
+}
+
+func (t azureTestClient) createNIC(resourceGroupName, machineName string, subnet *network.Subnet) *network.Interface {
+	interfacesClient := network.NewInterfacesClient(t.SubscriptionID)
+	interfacesClient.Authorizer = t.createServicePrincipalToken()
+
+	_, err := interfacesClient.CreateOrUpdate(resourceGroupName, fmt.Sprintf("nic-%s", machineName), network.Interface{
+		Location: &t.Location,
+		InterfacePropertiesFormat: &network.InterfacePropertiesFormat{
+			IPConfigurations: &[]network.InterfaceIPConfiguration{
+				{
+					Name: to.StringPtr(fmt.Sprintf("ip-config-%s", machineName)),
+					InterfaceIPConfigurationPropertiesFormat: &network.InterfaceIPConfigurationPropertiesFormat{
+						PrivateIPAllocationMethod: network.Dynamic,
+						Subnet: subnet,
+					},
+				},
+			},
+		},
+	}, nil)
+	Expect(err).NotTo(HaveOccurred())
+
+	nicParameters, err := interfacesClient.Get(resourceGroupName, fmt.Sprintf("nic-%s", machineName), "")
+	Expect(err).NotTo(HaveOccurred())
+
+	return &nicParameters
+}
+
+func (t azureTestClient) vmExists(identifier, resourceGroupName string) bool {
+	vmListResults, err := t.getVirtualMachinesClient().List(resourceGroupName)
+	if err != nil {
+		return false
+	}
+
+	var matchingInstances = make([]compute.VirtualMachine, 0)
+	var vmNameFilter = regexp.MustCompile(identifier)
+
+	for vmListResults.Value != nil && len(*vmListResults.Value) > 0 {
+		matchingInstances = getMatchingInstances(*vmListResults.Value, vmNameFilter, matchingInstances)
+		vmListResults, err = t.getVirtualMachinesClient().ListAllNextResults(vmListResults)
+		if err != nil {
+			return false
+		}
+	}
+
+	if len(matchingInstances) == 0 {
+		return false
+	}
+
+	return true
+}
+
+func createContainer(storageAccountName, storageAccountKey, containerName string) {
+	log.Printf("Creating container %s...\n", containerName)
+	client, err := storage.NewBasicClient(storageAccountName, storageAccountKey)
+	Expect(err).NotTo(HaveOccurred())
+
+	blobClient := client.GetBlobService()
+	cnt := blobClient.GetContainerReference(containerName)
+	err = cnt.Create()
+	Expect(err).NotTo(HaveOccurred())
+}
+
+func copyBlob(storageAccountName, storageAccountKey, containerName, sourceBlobURL, destinationBlobName string) string {
+	log.Printf("Copying %s to container %s with name %s...\n", sourceBlobURL, containerName, destinationBlobName)
+	client, err := storage.NewBasicClient(storageAccountName, storageAccountKey)
+	Expect(err).NotTo(HaveOccurred())
+
+	blobClient := client.GetBlobService()
+	err = blobClient.CopyBlob(containerName, destinationBlobName, sourceBlobURL)
+	Expect(err).NotTo(HaveOccurred())
+
+	return fmt.Sprintf("https://%s.blob.core.windows.net/%s/%s", storageAccountName, containerName, destinationBlobName)
+}
+
+func getMatchingInstances(vmList []compute.VirtualMachine, identifierRegex *regexp.Regexp, matchingInstances []compute.VirtualMachine) []compute.VirtualMachine {
+	for _, instance := range vmList {
+		if identifierRegex.MatchString(*instance.Name) {
+			matchingInstances = append(matchingInstances, instance)
+		}
+	}
+	return matchingInstances
+}
